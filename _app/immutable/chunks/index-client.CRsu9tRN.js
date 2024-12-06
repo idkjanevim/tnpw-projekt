a5ad7b81357d28942602914fa4d0ee5597976b07
Hrzán Ondřej <58387601+idkjanevim@users.noreply.github.com>
2024-12-06 20:32:23 +0100

    Updates

---
+++ _app/immutable/chunks/index-client.CRsu9tRN.js
@@ -0,0 +1,1 @@
+import{O as o,W as t,o as c,u}from"./runtime.Ck00RN1C.js";function a(e){throw new Error("lifecycle_outside_component")}function r(e){o===null&&a(),t&&o.l!==null?l(o).m.push(e):c(()=>{const n=u(e);if(typeof n=="function")return n})}function l(e){var n=e.l;return n.u??(n.u={a:[],b:[],m:[]})}export{r as o};
---
+++ _app/immutable/nodes/6.uLZDtN_g.js
@@ -0,0 +1,2 @@
+import{a,t as l}from"../chunks/disclose-version.g5D57kR9.js";import"../chunks/legacy.Dp8r9HO-.js";var i=l(`<div class="bg-primary text-text min-h-screen flex flex-col items-center justify-center text-center p-4"><div class="space-y-6 max-w-2xl"><h1 class="text-4xl md:text-6xl font-bold leading-tight">im sorry..</h1> <p class="text-lg md:text-xl text-muted leading-relaxed">this is a fake project :( 
+    i just needed to fill the project gallery a little</p></div></div>`);function x(t){var e=i();a(t,e)}export{x as component};
